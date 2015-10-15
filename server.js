43b6ffa1c99dd0c70c71af856005b2068a51c65f
unknown <lizandroal.zerpa@gmail.com>
2015-10-15 18:31:30 -0430

    Nuevo intento 2

--- server.js
+++ server.js
@@ -224,6 +224,6 @@ app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
 
 http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
-    console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
+    console.log("Express server listening at %s:%d ", app.get('ip'),app.get('port'));
     server();
 });
