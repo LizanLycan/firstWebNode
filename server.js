d7d0f67712bf2de31994bedebf4fb5ccf03a6a61
unknown <lizandroal.zerpa@gmail.com>
2015-10-16 20:56:39 -0430

    Intentos

--- server.js
+++ server.js
@@ -225,5 +225,5 @@ app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
 http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
     console.log("Express server listening at %s:%d ", app.get('ip'),app.get('port'));
-    server();
+    //server();
 });
