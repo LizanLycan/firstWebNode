e14231ac4a7ebf240aa11641290576cdb144edc1
unknown <lizandroal.zerpa@gmail.com>
2015-10-16 22:08:42 -0430

    otro intentsso

--- server.js
+++ server.js
@@ -216,14 +216,14 @@ app.get("/menu/new", function(solicitud, respuesta){
     respuesta.render("menu/new");
 });
 
-//var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-//var ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-//app.listen(port,ip);
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+app.listen(port,ip);
+/*app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
 
 http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
     console.log("Express server listening at %s:%d ", app.get('ip'),app.get('port'));
     //server();
-});
+});*/
